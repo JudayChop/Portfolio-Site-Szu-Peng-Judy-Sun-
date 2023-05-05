58f18a700f6ffe6241f351c45f06ad556b626731
Szu Peng Judy Sun <124474473+JudayChop@users.noreply.github.com>
2023-05-05 17:05:20 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-console.log ("Hello, Welcome to my Portfolio Page!")
+console.log("Hello, Welcome to my Portfolio Page!");
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-        x.className += ' responsive';
-      } else {
-        x.className = 'navtoggle';
-      }
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
 }
